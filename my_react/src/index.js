49ca8b0bad985da0063edef6f23c4ef94260bcb4
keanu912 <les_912@163.com>
2017-04-12 16:35:17 +0800

    build

---
+++ my_react/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+/*ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+*/
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
